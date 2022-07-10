32936aa3b101d58e86131f7c2f6c7048619c0939
Philip <johnphilipravina@gmail.com>
2022-07-11 07:36:37 +0800

    Fixed again.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -6,20 +8,15 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
+
+#app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'phil'
 api = Api(app)
-
-@app.before_first_request #flask decorator
-#this will run first over all the request
-
-def create_tables():
-    db.create_all()
 
 #SQLAlchemy creates the table for us using the code above.
 #the above code looks for the resource to find the table it needs to create.
@@ -46,6 +43,7 @@ api.add_resource(ItemList,'/items')
 api.add_resource(Item,'/item/<string:name>') #http://127.0.0:5000/item/<string:name>
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
     
