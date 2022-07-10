ec61729148fdf1b7208a4c1581747d02aa068051
Philip <johnphilipravina@gmail.com>
2022-07-11 01:45:02 +0800

    Fixed something.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -45,7 +46,6 @@ api.add_resource(ItemList,'/items')
 api.add_resource(Item,'/item/<string:name>') #http://127.0.0:5000/item/<string:name>
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
     
